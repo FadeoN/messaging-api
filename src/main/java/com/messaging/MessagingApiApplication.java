bea0bb80ae78889a4f05b77bffac651c1495b2a9
onur <onurfatih08@hotmail.com>
2020-10-02 02:14:52 +0300

    ADDED: Error logging added, default file logfile.log.

--- src/main/java/com/messaging/MessagingApiApplication.java
+++ src/main/java/com/messaging/MessagingApiApplication.java
@@ -1,12 +1,18 @@
 package com.messaging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessagingApiApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessagingApiApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(MessagingApiApplication.class, args);
+        logger.info("Simple log statement with inputs {}, {} and {}", 1,2,3);
+
     }
 }
